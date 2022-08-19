442f9bdc7ef6c2ce4b134d8545d18ed25e1b93b0
PavelKRY <kryukov.p14@gmail.com>
2022-08-19 18:19:50 +0500

    02 task issue

---
+++ Homework_01_task_02/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите чему равно а");
+string numberA = Console.ReadLine();
+Console.WriteLine("Введите чему равно b");
+string numberB = Console.ReadLine();
+Console.Write("Максимальное число это: ");
+    if (Convert.ToInt32(numberA) > Convert.ToInt32(numberB))
+        {
+        Console.WriteLine(Convert.ToInt32(numberA));
+        }
+    else
+        {
+        Console.WriteLine(Convert.ToInt32(numberB));
+        }
