1818dcf8a29b3ead2d021c03feda0a0c9391fd9b
PavelKRY <kryukov.p14@gmail.com>
2022-08-19 14:44:15 +0500

    issue resolved

--- A_task_of_two_friend_and_dog/Program.cs
+++ A_task_of_two_friend_and_dog/Program.cs
@@ -1,2 +1,25 @@
 ﻿int firstfFiendSpeed = 1;
-int secondFriendSpeed = 2;
+int secondFriendspeed = 2;
+int dogSpeed = 5;
+int distance = 10000;
+int friend = 2;
+int count = 0;
+while(distance > 10) 
+    {
+        if( friend == 1 )
+        {
+            int time = distance / (firstfFiendSpeed + dogSpeed);
+            friend = 1;
+            distance = distance - (firstfFiendSpeed + secondFriendspeed)*time;
+        }
+        else
+        {
+            int time = distance / (secondFriendspeed + dogSpeed);
+            distance = distance - (firstfFiendSpeed + secondFriendspeed)*time;    
+        }
+
+    count = count + 1;
+    }
+Console.Write("Собака пробежит ");
+Console.Write(count);
+Console.Write(" раз.");
