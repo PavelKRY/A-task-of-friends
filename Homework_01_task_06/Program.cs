2a00b3e5e6a29d5e63e065ebc6e22bce37320713
PavelKRY <kryukov.p14@gmail.com>
2022-08-22 12:52:11 +0500

    task06 issue

---
+++ Homework_01_task_06/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите значение: ");
+string firstNumber = Console.ReadLine();
+int FirstNumber = Int32.Parse(firstNumber);
+if(FirstNumber%2 == 0) {
+    Console.WriteLine("Ваше значение чётное");
+}
+else {
+    Console.WriteLine("Ваше значение нечётное");
+}
