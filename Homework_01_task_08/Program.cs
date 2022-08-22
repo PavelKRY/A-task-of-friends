c8b5a1fb568055c0f81b0038d9f2d4cc59de2a39
PavelKRY <kryukov.p14@gmail.com>
2022-08-22 14:00:45 +0500

    task08 issue

---
+++ Homework_01_task_08/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите значение: ");
+string inputa = Console.ReadLine();
+int inputA = Int32.Parse(inputa);
+Console.WriteLine("Ваш числовой ряд до введённого вами значения: ");
+while (inputA > 1){
+    if(inputA %2 == 0) {
+        Console.WriteLine(inputA);
+    }
+    else{
+        Console.WriteLine(inputA-1);
+    }
+    inputA=inputA-2;
+}
+
